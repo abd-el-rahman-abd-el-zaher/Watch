c0905b417caf694f97a2d9d7942c9169667683c1
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-08-31 16:51:13 +0200

    update diagrams

---
+++ DatabaseLayer/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DatabaseLayer.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=watch;trusted_connection=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
