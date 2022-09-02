4893490b889a13aa59a4bef9837c193ad1e7b10e
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-09-02 16:54:07 +0200

    Anime Model

--- DatabaseLayer/Data/AppDbContext.cs
+++ DatabaseLayer/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace DatabaseLayer.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext:IdentityDbContext
     {
         public AppDbContext()
         {
---
+++ DatabaseLayer/Models/Anime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer.Models
+{
+    public class Anime
+    {
+        public int AnimeId { get; set; }
+        public string Name { get; set; } 
+        public string Story { get; set; }
+        public string TeaserLink { get; set; }
+        public DayOfWeek OutDay { get; set; }
+        public string MalPage { get; set; }
+        public string ReleaseDate { get; set; }
+        public int EpisodesNumber { get; set; }
+        public string Image { get; set; }
+        public string Banner { get; set; }
+        
+    }
+}
