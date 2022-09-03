fe19eb7c79362beb51ce1661706506f7a3463066
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-09-03 17:00:47 +0200

    add models

---
+++ DatabaseLayer/Models/Episode.cs
@@ -0,0 +1,12 @@
+﻿using DatabaseLayer.Helper;
+namespace DatabaseLayer.Models
+{
+    public class Episode
+    {
+        public int EpisodeId { get; set; }
+        public string Title { get; set; }
+        public int WatchCount { get; set; }
+        public Filler Status { get; set; }
+        
+    }
+}
