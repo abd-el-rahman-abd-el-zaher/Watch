b26642a42a6126be86605afd9b990ee03955ffc5
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-09-08 22:22:34 +0200

    url validtion

--- API Layer/DataModels/NewAnime.cs
+++ API Layer/DataModels/NewAnime.cs
@@ -5,17 +5,19 @@ namespace API_Layer.DataModels
     public class NewAnime
     {
 
-        [Required(ErrorMessage = "Please Enter The Anime name ok im wating... ")]
+        [Required(ErrorMessage = "Please Enter The Anime name  ")]
         public string Name { get; set; }
         [Required]
         public string Story { get; set; }
         [Required]
-        [DataType(DataType.Url)]
+       // [DataType(DataType.Url)]
+       [RegularExpression(@"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",ErrorMessage = "Please Enter a valid url  ")]
         public string TeaserLink { get; set; }
         [Required(ErrorMessage = "Please Select the Out Day")]
         public DayOfWeek OutDay { get; set; }
         [Required]
-        [DataType(DataType.Url)]
+        //[DataType(DataType.Url)]
+       [RegularExpression(@"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",ErrorMessage = "Please Enter a valid url  ")]
         public string MalPage { get; set; }
         [Required]
         [DataType(DataType.Date)]
